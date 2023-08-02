5e3f015150cb8be09f3b9c0357d8fe8443a9845c
minje <alswp0910@naver.com>
2023-08-02 10:42:09 +0900

    map api 삭제

--- src/main/java/com/example/finalexamplespring/user/controller/JoinController.java
+++ src/main/java/com/example/finalexamplespring/user/controller/JoinController.java
@@ -31,43 +31,4 @@ public class JoinController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("인증 코드 생성 및 이메일 전송 실패");
         }
     }
-
-    @GetMapping("/map/{address}")
-    public void showMap(@PathVariable("address") String address) {
-        String apiKey = "1B5047DB-E0E8-34A5-856E-2E6118507460";
-        String service = "address";
-        String request = "getcoord";
-        String version = "2.0";
-        String crs = "epsg:4326";
-        boolean refine = true;
-        boolean simple = false;
-        String format = "Json";
-        String type = "road";
-
-        StringBuilder urlBuilder = new StringBuilder("https://api.vworld.kr/req/address?");
-        urlBuilder.append("service=").append(service)
-                .append("&request=").append(request)
-                .append("&version=").append(version)
-                .append("&crs=").append(crs)
-                .append("&address=").append(address)
-                .append("&refine=").append(refine)
-                .append("&simple=").append(simple)
-                .append("&format=").append(format)
-                .append("&type=").append(type)
-                .append("&key=").append(apiKey);
-
-        String url = urlBuilder.toString();
-
-        // Create a RestTemplate
-        RestTemplate restTemplate = new RestTemplate();
-
-        // HTTP GET request
-        String response = restTemplate.getForObject(url, String.class);
-
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
-
-        // Print the response
-        System.out.println("Response data:");
-        System.out.println(responseEntity.getBody());
-    }
 }
